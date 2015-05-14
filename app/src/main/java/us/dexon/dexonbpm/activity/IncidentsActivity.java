91acf1b0470dfbcf0840ceb7af59b3d3541fbdf2
Cesar Augusto Quiroz Zora <cesarqz86@gmail.com>
2015-05-14 16:00:04 -0500

    Change password feature: - Adding code to call the change password activity

--- app/src/main/java/us/dexon/dexonbpm/activity/IncidentsActivity.java
+++ app/src/main/java/us/dexon/dexonbpm/activity/IncidentsActivity.java
@@ -1,5 +1,6 @@
 package us.dexon.dexonbpm.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.ContextMenu;
@@ -55,7 +56,8 @@ public class IncidentsActivity extends FragmentActivity {
         // asignarle aquí el texto en función de la opción seleccionada.
         switch (item.getItemId()) {
             case R.id.button2_menu_opt1:
-                //accion para cambiar contraseña
+                Intent changePassIntent = new Intent(this, ChangePasswordActivity.class);
+                this.startActivity(changePassIntent);
                 return true;
             case R.id.button2_menu_opt2:
                 //accion para salir
